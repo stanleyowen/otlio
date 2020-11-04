4efe9d5e2fbe3ccb26638c7f0c5ea4b8bd91ae06
stanleyowen <69080584+stanleyowen@users.noreply.github.com>
2020-11-04 12:13:18 +0700

    Done

--- backend/routes/todo.routes.js
+++ backend/routes/todo.routes.js
@@ -11,6 +11,9 @@ router.route('/add').post((req,res) => {
     const username = req.body.username;
     const description = req.body.description;
     const date = Date.parse(req.body.date);
+    if(!username) {
+        return res.status('401').json('Unauthorized');
+    }
     const newExercise = new Exercise({
         username: username,
         description: description,
--- src/components/footer.components.js
+++ src/components/footer.components.js
@@ -3,7 +3,7 @@ export default class Footer extends Component {
     render() { 
         return (
             <footer id="footer" className="bg-primary text-white fixed-bottom">
-                <p>&copy; 2020 todo-stanley.herokuapp.com - All Rights Reserved | Created by <a href="http://stanleyowen.atwebpages.com" target="_blank" rel="noreferrer">Stanley Owen</a></p>
+                <p>&copy; 2020 - All Rights Reserved | Created by <a href="http://stanleyowen.atwebpages.com" target="_blank" rel="noreferrer">Stanley Owen</a></p>
             </footer>
         );
     }
