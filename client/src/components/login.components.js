60306b40ac6aa4a3ad8fed0db9d375c971d29371
stanleyowen <69080584+stanleyowen@users.noreply.github.com>
2021-03-03 15:08:10 +0700

    Added Show Password Feature (#40)

--- client/src/components/login.components.js
+++ client/src/components/login.components.js
@@ -9,28 +9,26 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [honeypot, setHoneypot] = useState('');
+    const [visible, setVisible] = useState(false);
 
     const Submit = (e) => {
         e.preventDefault();
         const btn = document.getElementById('login');
         async function submitData(){
             btn.innerHTML = "Logging In...";
-            const userData = { email, password }
-            await axios.post(`${SERVER_URL}/data/accounts/login`, userData)
+            await axios.post(`${SERVER_URL}/data/accounts/login`, { email, password })
             .then(res => {
-                localStorage.setItem('__id', res.data.id)
+                localStorage.setItem('__id', res.data.id);
                 localStorage.setItem('__token', res.data.token);
                 window.location = '/';
             })
-            .catch(err => {
-                setNotification(NOTIFICATION_TYPES.DANGER, err.response.data.message)
-            });
+            .catch(err => setNotification(NOTIFICATION_TYPES.DANGER, err.response.data.message));
             btn.removeAttribute("disabled");
             btn.classList.remove("disabled");
             btn.innerHTML = "Login";
         }
-        if(honeypot) { return }
-        else if(!email || !password){ setNotification(NOTIFICATION_TYPES.DANGER, 'Please Make Sure to Fill Out All the Required Fields !') }
+        if(honeypot) return;
+        else if(!email || !password) setNotification(NOTIFICATION_TYPES.DANGER, 'Please Make Sure to Fill Out All the Required Fields !')
         else if(EMAIL_VAL.test(String(email).toLocaleLowerCase()) === false){ setNotification(NOTIFICATION_TYPES.DANGER, 'Please Prvide a Valid Email Address !'); document.getElementById('email').focus(); }
         else { btn.setAttribute("disabled", "true"); btn.classList.add("disabled"); submitData(); }
     }
@@ -54,16 +52,19 @@ const Login = () => {
                             <div className="contact__formControl">
                                 <div className="contact__infoField">
                                     <label htmlFor="userEmail">Email</label>
-                                    <input title="Email" id="userEmail" type="email" className="contact__inputField" onChange={(event) => setEmail(event.target.value)} value={email} required autoFocus autoComplete="username"/>
+                                    <input title="Email" id="userEmail" type="email" className="contact__inputField" onChange={(event) => setEmail(event.target.value)} value={email} required autoFocus spellCheck="false" autoCapitalize="none" autoComplete="username"/>
                                     <span className="contact__onFocus"></span>
                                 </div>
                             </div>
                             <div className="contact__formControl">
                                 <div className="contact__infoField">
                                     <label htmlFor="userPassword">Password</label>
-                                    <input title="Password" id="userPassword" type="password" className="contact__inputField" onChange={(event) => setPassword(event.target.value)} value={password} required autoComplete="current-password"/>
+                                    <input title="Password" id="userPassword" type={ visible ? 'text':'password' } className="contact__inputField" onChange={(event) => setPassword(event.target.value)} value={password} required spellCheck="false" autoCapitalize="none" autoComplete="current-password"/>
                                     <span className="contact__onFocus"></span>
                                 </div>
+                            </div>
+                            <div className="contact__formControl">
+                                <input id="show-password" onClick={() => setVisible(!visible)} checked={visible} type="checkbox" /> <label for="show-password">Show Pasword</label>
                             </div>
                             <p style={{textAlign: 'center'}}>Haven't have an Account? <a className="animation__underline" href="/get-started">Get Started</a></p>
                             <button type="submit" className="contact__sendBtn" id="login">Login</button>
