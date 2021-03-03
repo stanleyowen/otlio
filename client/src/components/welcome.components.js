dd23095f864e3e2cc74f8196753253e6fdffcf91
stanleyowen <69080584+stanleyowen@users.noreply.github.com>
2021-03-03 12:21:52 +0700

    Optimized Performance

--- client/src/components/welcome.components.js
+++ client/src/components/welcome.components.js
@@ -5,40 +5,29 @@ import axios from 'axios';
 const GITHUB_API = "https://api.github.com/repos/stanleyowen/todo-application";
 
 const Landing = () => {
-    const [star, setStar] = useState('');
-    const [license, setLicense] = useState('');
+    const [repoInfo, setRepoInfo] = useState([]);
     const currentversion = process.env.REACT_APP_VERSION;
+
     useEffect(() => {
         async function getRepoInfo() {
-            await axios.get(`${GITHUB_API}`)
-            .then(res => {
-                if(res && res.data.stargazers_count && res.data.license.spdx_id){
-                    setStar(res.data.stargazers_count);
-                    setLicense(res.data.license.spdx_id);
-                }
-            })
+            await axios.get(GITHUB_API)
+            .then(res => setRepoInfo([res.data.stargazers_count, res.data.license.spdx_id]))
             .catch(err => {
-                if(err && err.response.data.message){
-                    setNotification(NOTIFICATION_TYPES.DANGER, 'ERR: '+err.response.data.message);
-                    setStar('Err'); setLicense('Err');
-                }else {
-                    setNotification(NOTIFICATION_TYPES.DANGER, "ERR: Invalid API");
-                    setStar('Err'); setLicense('Err');
-                }
+                if(err.response.data.message) setNotification(NOTIFICATION_TYPES.DANGER, 'ERR: '+err.response.data.message)
+                else setNotification(NOTIFICATION_TYPES.DANGER, "ERR: Invalid API")
             });
         }
         async function getLatestVersion() {
             await axios.get(`${GITHUB_API}/releases`)
             .then(res => {
-                if(res && res.data[0].tag_name){
+                if(res.data[0].tag_name){
                     let latestVersion =  res.data[0].tag_name.substring(1);
                     if(currentversion !== latestVersion) setNotification(NOTIFICATION_TYPES.WARNING, `Version ${latestVersion} is available`)
                 }
             })
             .catch(err => {
-                if(err && err.response.data.message){
-                    setNotification(NOTIFICATION_TYPES.DANGER, 'ERR: '+err.response.data.message);
-                }
+                if(err.response.data.message) setNotification(NOTIFICATION_TYPES.DANGER, 'ERR: '+err.response.data.message)
+                else setNotification(NOTIFICATION_TYPES.DANGER, "ERR: Couldn't Check for Updates")
             });
         }
         getRepoInfo();
@@ -52,8 +41,8 @@ const Landing = () => {
                 <a href="get-started" className="btn__outline">Get Started</a>
             </div>
             <div className="isCentered badges">
-                <a href="https://github.com/stanleyowen/TodoApp/"><button className="btn__label">License</button><button className="btn__value">{license}</button></a>
-                <a href="https://github.com/stanleyowen/TodoApp/stargazers"><button className="btn__label">Stars</button><button className="btn__value">{star}</button></a>
+                <a href="https://github.com/stanleyowen/TodoApp/"><button className="btn__label">License</button><button className="btn__value">{repoInfo[0]}</button></a>
+                <a href="https://github.com/stanleyowen/TodoApp/stargazers"><button className="btn__label">Stars</button><button className="btn__value">{repoInfo[1]}</button></a>
                 <a href="https://github.com/stanleyowen/todo-application/releases"><button className="btn__label">Version</button><button className="btn__value">{currentversion}</button></a>
             </div>
         </div>
--- client/src/library/getUserToken.js
+++ client/src/library/getUserToken.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
-const getUserToken = async (token, userId) => {
-    if(token){
+const getUserToken = async (token, id) => {
+    if(token && id){
         let _userInfo = null;
-        await axios.get(`${SERVER_URL}/data/accounts/getUserByToken`, {params: {id: userId, token}, headers: { Authorization: `JWT ${token}` }})
+        await axios.get(`${SERVER_URL}/data/accounts/getUserByToken`, {params: {id, token}, headers: { Authorization: `JWT ${token}` }})
         .then(res => _userInfo = res.data)
-        .catch(err => console.log(err));
+        .catch(() => _userInfo = null)
         return _userInfo;
     }else return undefined;
 }
