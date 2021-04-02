55576053ad7b8cb0d67dcb30e8949b2bf1253d7c
stanleyowen <69080584+stanleyowen@users.noreply.github.com>
2021-04-02 08:03:37 +0700

    Restructured Reset Password Component

--- client/src/components/reset-password.component.js
+++ client/src/components/reset-password.component.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { setNotification, NOTIFICATION_TYPES } from '../libraries/setNotification';
-import { getCSRFToken } from '../libraries/validation';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
+
+import { getCSRFToken } from '../libraries/validation';
+import { setNotification, NOTIFICATION_TYPES } from '../libraries/setNotification';
 
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 const EMAIL_VAL = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const ResetPassword = () => {
-    const parameter = window.location.search.slice(1).split('&');
-    const token = parameter[0].split('=')[1];
-    const id = parameter[1].split('=')[1];
+    const {id, token} = useParams();
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
     const [confirmPsw, setConfirmPsw] = useState();
@@ -23,31 +23,34 @@ const ResetPassword = () => {
             .catch(err => {
                 if(err.response.data.message || err.response.data.error_description){
                     setNotification(NOTIFICATION_TYPES.DANGER, err.response.data.message ? err.response.data.message : err.response.data.error_description);
-                    setTimeout(() => { window.location='/' }, 5000)
+                    setTimeout(() => { window.location='/reset-password' }, 5000)
                 }else window.location='/'
             })
         }
         validateData();
-    },[email])
+    },[id, token, email])
 
     const Submit = (e) => {
         e.preventDefault();
-        const btn = document.getElementById('register');
+        const btn = document.getElementById('reset-password');
         async function submitData(){
-            btn.innerHTML = "Resetting...";
-            const registerData = { token, id, email, password, confirmPassword: confirmPsw }
-            await axios.post(`${SERVER_URL}/account/reset-password`, registerData, { headers: { 'X-CSRF-TOKEN': getCSRFToken()[0], 'X-XSRF-TOKEN': getCSRFToken()[1] }, withCredentials: true })
+            btn.innerHTML = "Changing...";
+            const data = { token, id, email, password, confirmPassword: confirmPsw }
+            await axios.post(`${SERVER_URL}/account/reset-password`, data, { headers: { 'X-CSRF-TOKEN': getCSRFToken()[0], 'X-XSRF-TOKEN': getCSRFToken()[1] }, withCredentials: true })
             .then(() => window.location = '/')
-            .catch(err => setNotification(NOTIFICATION_TYPES.DANGER, err.response.data.message));
+            .catch(err => {
+                setNotification(NOTIFICATION_TYPES.DANGER, err.response.data.message);
+                setTimeout(() => { window.location='/reset-password' }, 5000)
+            });
             btn.removeAttribute("disabled");
             btn.classList.remove("disabled");
-            btn.innerHTML = "Reset";
+            btn.innerHTML = "Change Password";
         }
         if(honeypot) { return }
         else if(!email || !password || !confirmPsw){ setNotification(NOTIFICATION_TYPES.DANGER, 'Please Make Sure to Fill Out All the Required Fields !') }
         else if(EMAIL_VAL.test(String(email).toLocaleLowerCase()) === false){ setNotification(NOTIFICATION_TYPES.DANGER, 'Please Prvide a Valid Email Address !'); document.getElementById('validatedEmail').focus(); }
         else if(email.length < 6 || email.length > 40){ setNotification(NOTIFICATION_TYPES.DANGER, 'Please Provide an Email between 6 ~ 40 characters !'); document.getElementById('validatedEmail').focus(); }
-        else if(password.length < 6 || password.length > 40){ setNotification(NOTIFICATION_TYPES.DANGER, 'Please Provide a Password between 6 ~ 40 characters !'); document.getElementById('password').focus(); }
+        else if(password.length < 6 || password.length > 40 || confirmPsw.length < 6 || confirmPsw.length > 40){ setNotification(NOTIFICATION_TYPES.DANGER, 'Please Provide a Password between 6 ~ 40 characters !'); document.getElementById('password').focus(); }
         else if(password !== confirmPsw){ setNotification(NOTIFICATION_TYPES.DANGER, 'Please Make Sure Both Passwords are Match !'); document.getElementById('password').focus(); }
         else { btn.setAttribute("disabled", "true"); btn.classList.add("disabled"); submitData(); }
     }
@@ -60,14 +63,14 @@ const ResetPassword = () => {
                     <form className="contact__form" name="contact__form" onSubmit={Submit}>
                         <div className="contact__formControl no-bot">
                             <div className="contact__infoField">
-                                <label htmlFor="bot-validatedEmail">Email <span className="required">*</span></label>
+                                <label htmlFor="bot-validatedEmail">Email</label>
                                 <input title="Email" id="bot-validatedEmail" type="text" className="contact__inputField" onChange={(event) => setHoneypot(event.target.value)} value={honeypot} autoComplete="off"/>
                                 <span className="contact__onFocus"></span>
                             </div>
                         </div>
                         <div className="contact__formControl">
                             <div className="contact__infoField">
-                                <label htmlFor="email">Email <span className="required">*</span></label>
+                                <label htmlFor="email">Email</label>
                                 <input title="Email" id="email" type="email" className="contact__inputField" value={email} autoFocus required disabled="true" autoComplete="username"/>
                                 <span className="contact__onFocus"></span>
                             </div>
@@ -87,11 +90,11 @@ const ResetPassword = () => {
                                     <span className="contact__onFocus"></span>
                                 </div>
                             </div>
-                            <div className="contact__formControl show-password">
-                                <input id="show-oauth-password" onClick={() => setVisible(!visible)} type="checkbox" /> <label htmlFor="show-oauth-password">Show Pasword</label>
-                            </div>
                         </div>
-                        <button type="submit" className="contact__sendBtn" id="register">Reset</button>
+                        <div className="contact__formControl show-password">
+                            <input id="show-oauth-password" onClick={() => setVisible(!visible)} type="checkbox" /> <label htmlFor="show-oauth-password">Show Pasword</label>
+                        </div>
+                        <button type="submit" className="contact__sendBtn" id="reset-password">Change Password</button>
                     </form>
                 </div>
             </div>
