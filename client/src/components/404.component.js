5093fd2497a130a977f350bc7f99e9bc03bb299c
Stanley Owen <stanleyowen06@gmail.com>
2021-05-22 12:04:05 +0700

    Improved User Interface

--- client/src/components/404.component.js
+++ client/src/components/404.component.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faChartLine } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { Tooltip } from '@material-ui/core'
 import Image from '../img/4f4adcbf8c6f66dcfc8a3282ac2bf10a.webp'
@@ -14,13 +14,16 @@ const PageNotFound = ({ userData }) => {
                     <h1 className="blue-text monospace large">404</h1>
                     <h1 className="blue-text monospace">Page Not Found</h1>
                     <h3 className="mt-20 monospace">Oops! We are sorry, we can't find the page you were looking for.</h3>
-                    <h3 className="mt-20 monospace">If you're experiencing a critical issue, please <a className="animation__underline" href="mailto:stanleyowen06@gmail.com">email support</a>.</h3>
+                    <h3 className="mt-20 monospace">If you're experiencing a critical issue, please <a className="animation__underline" href="/contact">contact support</a>.</h3>
                     <button className="oauth-box google isCentered block mt-30 mb-20 p-12 button" onClick={() => userData.authenticated ? window.location='/' : window.location='/welcome'}>Back to Home</button>
                     <div className="footer__socialBtn mb-20">
-                        <Tooltip title="Email Support" arrow><a href="mailto:stanleyowen06@gmail.com">
+                        <Tooltip title="Contact Support" arrow><a href="/support" rel="noopener noreferrer">
                             <FontAwesomeIcon icon={faEnvelope} style={{ fontSize: "1.8em" }} />
                         </a></Tooltip>
-                        <Tooltip title="View Code on GitHub" arrow><a href="https://github.com/stanleyowen/todo-application/">
+                        <Tooltip title="Current Status" arrow><a href="https://todoapp.freshstatus.io/" target="_blank" rel="noreferrer">
+                            <FontAwesomeIcon icon={faChartLine} style={{ fontSize: "1.8em" }} />
+                        </a></Tooltip>
+                        <Tooltip title="View Code on GitHub" arrow><a href="https://github.com/stanleyowen/todo-application/" target="_blank" rel="noreferrer">
                             <FontAwesomeIcon icon={faGithub} style={{ fontSize: "1.8em" }} />
                         </a></Tooltip>
                     </div>
--- client/src/components/support.component.js
+++ client/src/components/support.component.js
@@ -2,10 +2,11 @@ import axios from 'axios'
 import React, { useState } from 'react'
 import { Select, MenuItem } from '@material-ui/core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faChartLine, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { Tooltip } from '@material-ui/core'
 import Image from '../img/765cbf066c6e4c42444a0ce9c2fb7949.webp'
+import MessageSent from '../img/2405976c7c1485050e78fcf54ca4bfe4.webp'
 
 import { NOTIFICATION_TYPES, setNotification } from '../libraries/setNotification'
 import { getCSRFToken } from '../libraries/validation'
@@ -29,7 +30,8 @@ const Support = ({ userData }) => {
         description: ''
     })
     const [properties, setProperties] = useState({
-        honeypot: ''
+        honeypot: '',
+        messageSent: false
     })
 
     const handleChange = (a, b) => setProperties({ ...properties, [a]: b })
@@ -45,12 +47,31 @@ const Support = ({ userData }) => {
 
             <div className="main">
                 <div className="contact__container">
-                    <img className="center-object" src={Image} alt="Contact Us" />
-                    <div id="form no-padding">
-                        <div className="form__contact">
-                            <div className="get_in_touch"><h1 className="monospace blue-text">Contact Us</h1></div>
+                    <div className="center-object">
+                        <img src={properties.messageSent ? MessageSent : Image} alt="Contact Us" />
+                        <div className="footer__socialBtn mt-20 mb-20">
+                            <Tooltip title="Contact Support" arrow><a href="/support" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faEnvelope} style={{ fontSize: "1.8em" }} />
+                            </a></Tooltip>
+                            <Tooltip title="Current Status" arrow><a href="https://todoapp.freshstatus.io/" target="_blank" rel="noreferrer">
+                                <FontAwesomeIcon icon={faChartLine} style={{ fontSize: "1.8em" }} />
+                            </a></Tooltip>
+                            <Tooltip title="View Code on GitHub" arrow><a href="https://github.com/stanleyowen/todo-application/" target="_blank" rel="noreferrer">
+                                <FontAwesomeIcon icon={faGithub} style={{ fontSize: "1.8em" }} />
+                            </a></Tooltip>
+                        </div>
+                    </div>
+                    {properties.messageSent ?
+                    (<div className="center-object">
+                        <h1 className="blue-text monospace">Message Sent</h1>
+                        <h3 className="mt-20 monospace">Thank you for submitting the details. We have received your issue(s) and or enhancement(s), and you should receive an email from us after 48 hours.</h3>
+                        <button className="oauth-box google isCentered block mt-30 mb-20 p-12 button" onClick={() => userData.authenticated ? window.location='/' : window.location='/welcome'}>Back to Home</button>
+                    </div>) :
+                    (<div className="no-padding mb-20">
+                        <div className="form__contact no-margin">
+                            <div className="get_in_touch"><h1 className="monospace blue-text">{properties.messageSent ? 'Message Sent':'Contact Us'}</h1></div>
                             <div className="form">
-                                <form className="contact__form" name="contact__form">
+                                <form className="contact__form" name="contact__form" onSubmit={submitTicket}>
                                     <div className="m-10 no-bot">
                                         <div className="contact__infoField">
                                             <label htmlFor="bot-email">Email</label>
@@ -84,7 +105,7 @@ const Support = ({ userData }) => {
                                     <div className="m-10 mt-20">
                                         <div className="contact__infoField">
                                             <label htmlFor="description">Description</label>
-                                            <textarea id="description" className="contact__inputField resize-y" rows="5" minLength="30" maxLength="1000" onChange={(e) => handleData('description', e.target.value)} value={data.description} required placeholder="Describe your issue as clearly as you can"></textarea>
+                                            <textarea id="description" className="contact__inputField resize-y" rows="4" minLength="30" maxLength="1000" onChange={(e) => handleData('description', e.target.value)} value={data.description} required placeholder="Describe your issue as clearly as you can"></textarea>
                                             <span className="contact__onFocus"></span>
                                             <p className="length">{data.description.length}/1000</p>
                                         </div>
@@ -94,7 +115,7 @@ const Support = ({ userData }) => {
                                 </form>
                             </div>
                         </div>
-                    </div>
+                    </div>) }
                 </div>
             </div>
         </div>
