56b3cede098c82975e41884540f52df261c20f66
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2021-08-03 16:29:11 +0700

    Import Images from React

--- client/src/libraries/image.js
+++ client/src/libraries/image.js
@@ -1,3 +1,13 @@
+import local2FA from '../img/d0d4cf7a79acbcb98834d05980edc55d.webp'
+import localCloud from '../img/af051c89597cd018ce51bd8fd53014ff.webp'
+import localGithub from '../img/589612f86db2a2b483b007bc2a1e9665.webp'
+import localSupport from '../img/95cc64dd2825f9df13ec4ad683ecf339.webp'
+import localNotFound from '../img/4f4adcbf8c6f66dcfc8a3282ac2bf10a.webp'
+import localSecurity from '../img/e91e6348157868de9dd8b25c81aebfb9.webp'
+import localMessageSent from '../img/2405976c7c1485050e78fcf54ca4bfe4.webp'
+import localCustomerSupport from '../img/765cbf066c6e4c42444a0ce9c2fb7949.webp'
+import localOrganizingEasier from '../img/7c27535f88bae9519ceb14a8983c57ff.webp'
+
 const data = {
     "2FA": "https://user-images.githubusercontent.com/69080584/127344051-76c49390-9a92-437e-8bc7-67fbc49eef6c.png",
     cloud: "https://user-images.githubusercontent.com/69080584/127344393-3982f4dc-3ecf-417e-80bb-915ba18b2e50.png",
@@ -8,15 +18,15 @@ const data = {
     messageSent: "https://user-images.githubusercontent.com/69080584/127345328-c48c3979-c686-4230-ab2d-917d57734405.png",
     customerSupport: "https://user-images.githubusercontent.com/69080584/127345265-6dea5059-320c-42b2-9e1a-8bcc23c214d9.png",
     organizingEasier: "https://user-images.githubusercontent.com/69080584/127344230-854bca56-1410-4db3-8279-decf0cf4f96c.png",
-    local2FA: "/static/media/d0d4cf7a79acbcb98834d05980edc55d.webp",
-    localCloud: "/static/media/af051c89597cd018ce51bd8fd53014ff.webp",
-    localGithub: "/static/media/589612f86db2a2b483b007bc2a1e9665.webp",
-    localSupport: "/static/media/95cc64dd2825f9df13ec4ad683ecf339.webp",
-    localNotFound: "/static/media/af051c89597cd018ce51bd8fd53014ff.webp",
-    localSecurity: "/static/media/e91e6348157868de9dd8b25c81aebfb9.webp",
-    localMessageSent: "/static/media/2405976c7c1485050e78fcf54ca4bfe4.webp",
-    localCustomerSupport: "/static/media/765cbf066c6e4c42444a0ce9c2fb7949.webp",
-    localOrganizingEasier: "/static/media/7c27535f88bae9519ceb14a8983c57ff.webp"
+    local2FA,
+    localCloud,
+    localGithub,
+    localSupport,
+    localNotFound,
+    localSecurity,
+    localMessageSent,
+    localCustomerSupport,
+    localOrganizingEasier
 }
 
 export default data
